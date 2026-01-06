9960eb2c45a33c5086ca6474bbc2051d6dec1671
Your Name <you@example.com>
2026-01-06 14:08:56 +0000

    c

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -11,8 +11,8 @@ def fill(df):
     df = df.drop(columns=["Weighted_Price"])
     df['Close'] = df['Close'].ffill()
     df['High'] = df['High'].fillna(df['Close'])
-    df['Low'] = df['High'].fillna(df['Close'])
-    df['Open'] = df['High'].fillna(df['Close'])
+    df['Low'] = df['Low'].fillna(df['Close'])
+    df['Open'] = df['Open'].fillna(df['Close'])
     df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
     df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
     return df
