5b7a33b69c16fef30e965a35e3d5037ed1c63f97
Your Name <you@example.com>
2026-01-06 11:18:48 +0000

    c

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+import pandas as pd
+
+
+dc = {"First": [0.0, 0.5, 1.0, 1.5], "Second": ["one", "two", "three", "four"]}
+df = pd.DataFrame.from_dict(dc, orient='columns')
