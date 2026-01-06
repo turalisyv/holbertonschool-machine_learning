b7cb495bf0b906519b8fe0710f4054b5847da6b1
Your Name <you@example.com>
2026-01-06 11:23:05 +0000

    c

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -7,3 +7,4 @@ import pandas as pd
 
 dc = {"First": [0.0, 0.5, 1.0, 1.5], "Second": ["one", "two", "three", "four"]}
 df = pd.DataFrame.from_dict(dc, orient='columns')
+df.index = ['A', 'B', 'C', 'D']
