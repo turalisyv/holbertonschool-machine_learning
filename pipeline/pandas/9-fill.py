5f3a66330a01e78b9bcaa14557cfe99ddc80bc99
Your Name <you@example.com>
2026-01-06 14:01:31 +0000

    c

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+
+
+def fill(df):
+    '''
+    My function document
+    '''
+    df['Close'] = df['Close'].ffill()
+    df['High'], df['Low'], df['Open'] = df['Close'], df['Close'], df['Close']
+    df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
+    df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
+    return df
