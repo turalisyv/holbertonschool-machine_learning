74056eefb559f25c2b47c074293ec0e3cef14a26
Your Name <you@example.com>
2026-01-06 11:28:04 +0000

    c

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    '''
+    My function document
+    '''
+    return pd.DataFrame.to_csv(filename, sep=delimiter)
