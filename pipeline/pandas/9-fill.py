cd5642be8c2e3dbf10276be5e0a389938ab9a522
Your Name <you@example.com>
2026-01-06 14:03:03 +0000

    c

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -8,6 +8,7 @@ def fill(df):
     '''
     My function document
     '''
+    df = df.drop(columns=["Weighted_Price"])
     df['Close'] = df['Close'].ffill()
     df['High'], df['Low'], df['Open'] = df['Close'], df['Close'], df['Close']
     df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
