0115e63e717f5fde8399f7ad2f5a33faefed26f4
Your Name <you@example.com>
2026-01-08 11:24:09 +0000

    c

---
+++ pipeline/pandas/14-visualize.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+
+df = df.rename(columns={"Timestamp": "Date"})
+df["Date"] = pd.to_datetime(df['Date'], unit='s')
+df = df[df["Date"] >= pd.Timestamp('2017-01-01')]
+df = df.set_index("Date")
+df = df.drop(columns=["Weighted_Price"])
+df['Close'] = df['Close'].ffill()
+df['High'] = df['High'].fillna(df['Close'])
+df['Low'] = df['Low'].fillna(df['Close'])
+df['Open'] = df['Open'].fillna(df['Close'])
+df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
+df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
+df.plot(kind="line")
