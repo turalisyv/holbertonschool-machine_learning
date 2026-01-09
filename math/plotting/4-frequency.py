ca053f23dc641a177df8706c22d71e2448414bb7
Your Name <you@example.com>
2026-01-09 11:52:29 +0000

    c

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -14,5 +14,8 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
+    plt.title("Project A")
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
     plt.hist(student_grades, range=(0, 100), edgecolor='black')
     plt.show()
