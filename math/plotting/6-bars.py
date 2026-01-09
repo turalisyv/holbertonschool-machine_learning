714371741811a6059612f24a184c02cd739601bd
Your Name <you@example.com>
2026-01-09 15:50:18 +0000

    c

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -11,7 +11,7 @@ def bars():
     My function documents
     '''
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
     bt = fruit[0]
     spc = ["Farrah", "Fred", "Felicia"]
