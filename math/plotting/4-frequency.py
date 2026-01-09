61135e3eeebd9ded25e1bdda89e427855a87b951
Your Name <you@example.com>
2026-01-09 11:55:34 +0000

    c

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -17,5 +17,7 @@ def frequency():
     plt.title("Project A")
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
     plt.hist(student_grades, range=(0, 100), edgecolor='black')
     plt.show()
