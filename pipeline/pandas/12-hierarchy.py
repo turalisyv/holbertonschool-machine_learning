9e5c275c414b08ea981b6d5609bdd1e0f9839d85
Your Name <you@example.com>
2026-01-09 11:04:05 +0000

    c

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -6,14 +6,16 @@ import pandas as pd
 index = __import__('10-index').index
 
 
-def concat(df1, df2):
-    '''
-    My function document
-    '''
+def hierarchy(df1, df2):
+    """A function that does the trick"""
     df1 = index(df1)
     df2 = index(df2)
-    df1 = df1.loc[1417411980:1417417980]
-    df2 = df2.loc[1417411980:1417417980]
-    df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
-    df = df.swaplevel(0, 1, axis=0).sort_index()
-    return df
+    df = pd.concat(
+        [
+            df2.loc[1417411980:1417417980],
+            df1.loc[1417411980:1417417980]
+        ],
+        keys=["bitstamp", "coinbase"]
+    )
+    df = df.swaplevel(0, 1, axis=0)
+    return df.sort_index()
