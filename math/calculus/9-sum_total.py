31fc2eb11634caa68be665cd918dde017b2ee7e9
Your Name <you@example.com>
2026-01-12 19:09:14 +0000

    c

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -7,5 +7,8 @@ def summation_i_squared(n):
     '''
     My function document
     '''
+    try:
+        return sum(map(lambda i: i**2, range(0, n+1)))
 
-    return sum(map(lambda i: i**2, range(0, n+1)))
+    except TypeError:
+        return None
