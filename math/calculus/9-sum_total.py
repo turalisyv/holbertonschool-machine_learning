d7557eda77cd437bd9abc1dcce8c600ddff17f7b
Your Name <you@example.com>
2026-01-12 19:06:06 +0000

    c

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+
+def summation_i_squared(n):
+    '''
+    My function document
+    '''
+
+    return sum(map(lambda i: i**2, range(0, n+1)))
