45bc5fbba10696c1e4aa9773ca4be78538066900
Your Name <you@example.com>
2026-01-12 19:24:03 +0000

    c

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+
+
+def poly_derivative(poly):
+    '''
+    My function document
+    '''
+    der = []
+
+    for i in range(1, len(poly)):
+        der.append(i * poly[i])
+
+    return der
