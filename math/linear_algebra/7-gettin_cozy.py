2bf76e765bf4192120372fe24358b644047956cf
Your Name <you@example.com>
2026-01-21 12:13:07 +0000

    c

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -8,7 +8,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     '''
     My function document
     '''
-    if len(mat1[0]) != len(mat2[0]):
+    if len(mat1) != len(mat2):
         return None
 
     mat3 = []
