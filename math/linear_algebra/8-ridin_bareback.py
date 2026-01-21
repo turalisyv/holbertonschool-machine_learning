93f44f5e3b23190596df2fc1140733dbb0a7d9f7
Your Name <you@example.com>
2026-01-21 15:02:45 +0000

    c

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -8,6 +8,9 @@ def mat_mul(mat1, mat2):
     '''
     My function document
     '''
+    if len(mat1[0]) != len(mat2):
+        return None
+
     mat3 = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
 
     for i in range(len(mat1)):
