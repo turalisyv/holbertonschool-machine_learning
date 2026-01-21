581dd818bacc1d85e3a3b526621c746c8635ac5e
Your Name <you@example.com>
2026-01-21 11:20:59 +0000

    c

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+
+
+def matrix_shape(matrix):
+    '''
+    My function document
+    '''
+    shape = []
+    while True:
+        shape.append(len(matrix))
+        matrix = matrix[0]
+
+      if isinstance(matrix, int):
+          break
+
+    return shape
