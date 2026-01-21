95c35ca8580c1634c96e89d97e024d5ed90b3aaa
Your Name <you@example.com>
2026-01-21 12:10:36 +0000

    c

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+M module document
+'''
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    '''
+    My function document
+    '''
+    mat3 = []
+    if axis == 0:
+        mat3 = mat1 + mat2
+
+    elif axis == 1:
+        for i in range(len(mat1)):
+            mat3.append(mat1[i] + mat2[i])
+
+    return mat3
