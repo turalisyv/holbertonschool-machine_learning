b03cef625bdb02015cade52a4593fff0cb76d5a2
Your Name <you@example.com>
2026-01-21 14:07:15 +0000

    c

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+
+
+def np_elementwise(mat1, mat2):
+    '''
+    My function document
+    '''
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
