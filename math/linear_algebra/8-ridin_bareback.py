e9629d219897137e68fb36f8e014e057a04b44c2
Your Name <you@example.com>
2026-01-21 14:56:39 +0000

    c

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+import numpy as np
+
+
+def mat_mul(mat1, mat2):
+    '''
+    My function document
+    '''
+    mat3 = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat1[0])):
+                mat3[i][j] += mat1[i][k] * mat2[k][j]
+
+    return mat3
