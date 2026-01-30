696b738552ff3f4d4a741fc29c0c2cb9c0b645c3
Your Name <you@example.com>
2026-01-30 13:45:38 +0000

    c

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,35 @@
+#!/bin/bash/env python3
+'''
+My module document
+'''
+
+
+def definiteness(matrix):
+    '''
+    My function document
+    '''
+
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+
+    try:
+        np.linalg.eigvals(matrix)
+    except Exception as e:
+        return None
+
+    A = np.linalg.eigvals(matrix)
+
+    if np.all(A > 0):
+        return "Positive definite"
+
+    elif np.all(A < 0):
+        return "Negative definite"
+
+    elif np.all(A >= 0):
+        return "Positive semi-definite"
+
+    elif np.all(A <= 0):
+        return "Negative semi-definite"
+
+    else:
+        return "Indefinite"
