8ae00d9d6bfd5b1ca5a5c0e7260ccdf1de1efd2c
Your Name <you@example.com>
2026-02-10 07:55:57 +0000

    c

---
+++ math/probability/normal.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+
+
+class Normal:
+    '''
+    My class document
+    '''
+    def __init__(self, data=None, mean=0., stddev=1.):
+        self.e = 2.7182818285
+        self.data = data
+
+        self.mean = mean
+
+        if stddev > 0:
+            self.stddev = stddev
+        else:
+            raise ValueError("stddev must be a positive value")
+
+        if self.data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            self.mean = sum(self.data) / len(self.data)
+
+            for i in self.data:
+                self.stddev += (i - self.mean)**2
+            self.stddev = np.sqrt(self.stddev /  len(self.data))
