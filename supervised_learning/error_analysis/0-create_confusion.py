efe4a55cfaf3ce346c7af25533328b66ddeb04df
Your Name <you@example.com>
2026-02-20 14:05:57 +0000

    c

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -9,13 +9,4 @@ def create_confusion_matrix(labels, logits):
     '''
     My function document
     '''
-    n = labels.shape[0]
-    conf_mat = np.zeros((n, n), dtype=np.int32)
-    row = np.where(labels == 1)[1]
-    col = np.where(logits == 1)[1]
-
-    for i, j in zip(row, col):
-        conf_mat[i, j] = conf_mat[i, j] + 1
-
-    np.set_printoptions(threshold=np.inf)
-    return conf_mat
+    return np.dot(labels.T, logits)
