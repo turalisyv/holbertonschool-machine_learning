43c78c739272186fdcf5b83289167083b3483320
turalisyv <isayev.tural.official@gmail.com>
2026-02-22 00:06:57 +0400

    Update 0-build_decision_tree.py

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -47,6 +47,9 @@ class Leaf(Node):
 
 
 class Decision_Tree():
+    """
+    Decision_Tree class
+    """
     def __init__(self, max_depth=10, min_pop=1,
                  seed=0, split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
