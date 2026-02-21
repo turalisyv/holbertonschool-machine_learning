1fdd4c937fe9089b6d546f4725d8984f9ac9db7e
turalisyv <isayev.tural.official@gmail.com>
2026-02-22 00:04:17 +0400

    Update 0-build_decision_tree.py

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -1,1 +1,65 @@
+#!/usr/bin/env python3
+"""
+My module document
+"""
+import numpy as np
 
+
+class Node:
+    def __init__(self, feature=None, threshold=None,
+                 left_child=None, right_child=None, is_root=False, depth=0):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """
+        max_depth_below function
+        """
+        left = self.left_child.max_depth_below()
+        right = self.right_child.max_depth_below()
+        return max(left, right)
+
+
+class Leaf(Node):
+    """
+    Leaf function
+    """
+    def __init__(self, value, depth=None):
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """
+        max_depth_below
+        """
+        return self.depth
+
+
+class Decision_Tree():
+    def __init__(self, max_depth=10, min_pop=1,
+                 seed=0, split_criterion="random", root=None):
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """
+        depth function
+        """
+        return self.root.max_depth_below()
