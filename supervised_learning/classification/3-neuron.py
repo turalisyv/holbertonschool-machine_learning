0ebc9aeda828557c3b207a132ab1b3df8bc58d93
Your Name <you@example.com>
2026-03-04 10:45:34 +0000

    c

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+import numpy as np
+
+
+class Neuron:
+    '''
+    My class document
+    '''
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    def sigmoid(self, x):
+        '''My function document'''
+        return 1 / (1 + np.exp(-x))
+
+    def forward_prop(self, X):
+        '''My functiion document'''
+        self.__A = self.sigmoid(np.dot(self.__W, X) + self.__b)
+        return self.__A
+    
+    def cost(self, Y, A):
+        '''My function document'''
+        m = Y.shape[1]
+        C = - (1 / m) * np.sum(
+            np.multiply(
+                Y, np.log(A)) + np.multiply(
+                1 - Y, np.log(1.0000001 - A)))
+        return C
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
