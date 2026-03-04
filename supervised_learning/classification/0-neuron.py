6994df41387488da5b9a5b173190904ff6366068
Your Name <you@example.com>
2026-03-04 09:04:34 +0000

    c

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -6,9 +6,16 @@ import numpy as np
 
 
 class Neuron:
+    '''
+    My class document
+    '''
     def __init__(self, nx):
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        
+
         if nx < 1:
             raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
