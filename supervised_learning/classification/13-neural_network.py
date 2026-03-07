b459c0264992e4312c13f6904ebb58423bd9808a
Your Name <you@example.com>
2026-03-07 12:23:15 +0000

    c

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -55,6 +55,23 @@ class NeuralNetwork:
         y_pred = np.int32(y_pred > 0.5)
         return y_pred, A
 
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        '''My function document'''
+        m = Y.shape[1]
+
+        dZ2 = A2 - Y
+        dW2 = (1 / m) * np.dot(dZ2, A1.T)
+        db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+
+        dZ1 = np.dot(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = (1 / m) * np.dot(dZ1, X.T)
+        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+
+        self.__W2 = self.__W2 - alpha * dW2
+        self.__b2 = self.__b2 - alpha * db2
+        self.__W1 = self.__W1 - alpha * dW1
+        self.__b1 = self.__b1 - alpha * db1
+
     @property
     def W1(self):
         return self.__W1
