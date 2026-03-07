ea57185722c88f09821d8bbfb1c08642c6832556
Your Name <you@example.com>
2026-03-07 17:57:00 +0000

    c

---
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    '''
+    My class document
+    '''
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.L):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+
+            if i == 0:
+                self.weights['W1'] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.weights[f'W{i + 1}'] = np.random.randn(
+                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+
+            self.weights[f'b{i + 1}'] = np.zeros((layers[i], 1))
+
+    def sigmoid(self, x):
+        '''My function document'''
+        return 1 / (1 + np.exp(-x))
+
+    def forward_prop(self, X):
+        '''My function document'''
+        self.__cache['A0'] = X
+        for i in range(1, self.__L + 1):
+            W = self.__weights['W' + str(i)]
+            b = self.__weights['b' + str(i)]
+            A_prev = self.__cache['A' + str(i - 1)]
+            Z = np.dot(W, A_prev) + b
+            A = self.sigmoid(Z)
+            self.__cache['A' + str(i)] = A
+        return self.__cache['A' + str(self.__L)], self.__cache
+
+    def cost(self, Y, A):
+        '''My function document'''
+        m = Y.shape[1]
+        C = - (1 / m) * np.sum(
+            np.multiply(
+                Y, np.log(A)) + np.multiply(
+                1 - Y, np.log(1.0000001 - A)))
+        return C
+
+def evaluate(self, X, Y):
+        A, _ = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
