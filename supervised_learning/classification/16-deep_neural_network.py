7ecee27561134cababfcf333283e111648dd41b9
Your Name <you@example.com>
2026-03-07 17:42:28 +0000

    c

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -29,6 +29,7 @@ class DeepNeuralNetwork:
             if i == 0:
                 self.weights['W1'] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
             else:
-                self.weights[f'W{i + 1}'] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+                self.weights[f'W{i + 1}'] = np.random.randn(layers[i], layers[i - 1]) 
+                * np.sqrt(2 / layers[i - 1])
 
             self.weights[f'b{i + 1}'] = np.zeros((layers[i], 1))
