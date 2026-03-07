79cc240c7a86bf960f9a9bd735ba31f512270416
Your Name <you@example.com>
2026-03-07 17:45:07 +0000

    c

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -14,7 +14,6 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
@@ -29,7 +28,6 @@ class DeepNeuralNetwork:
             if i == 0:
                 self.weights['W1'] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
             else:
-                self.weights[f'W{i + 1}'] = np.random.randn(layers[i], 
-                layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+                self.weights[f'W{i + 1}'] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
 
             self.weights[f'b{i + 1}'] = np.zeros((layers[i], 1))
