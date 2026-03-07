c91a9cd7205911eb9bc68059d807835b0ac164e1
Your Name <you@example.com>
2026-03-07 17:41:32 +0000

    c

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    '''
+    My class document
+    '''
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for i in range(self.L):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+
+            if i == 0:
+                self.weights['W1'] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.weights[f'W{i + 1}'] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+
+            self.weights[f'b{i + 1}'] = np.zeros((layers[i], 1))
