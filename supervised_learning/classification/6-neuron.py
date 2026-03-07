317977999265c6ee6d5b6bb9f795807323d5e661
Your Name <you@example.com>
2026-03-07 10:21:54 +0000

    c

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -57,6 +57,19 @@ class Neuron:
         self.__b = self.__b - alpha * db
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
+        '''My function document'''
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+
+        if iterations < 0:
+            raise ValueError("iterations must be a positive integer")
+
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
+
         for iter in range(iterations):
             self.forward_prop(X)
             self.gradient_descent(X, Y, self.A, alpha=alpha)
