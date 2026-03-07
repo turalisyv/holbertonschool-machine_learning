44c29b6ab30b498b82b065fd2559282e4f7d0fa0
Your Name <you@example.com>
2026-03-07 18:09:52 +0000

    c

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    '''
+    My function document
+    '''
+    res = np.zeros(shape=(len(Y), classes))
+
+    for i in range(len(Y)):
+        res[i][Y[i]] = 1
+
+    return res
