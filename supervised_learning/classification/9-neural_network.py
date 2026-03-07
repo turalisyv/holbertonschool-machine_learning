0db1e7370111345298fef3607dcc1c1a2ffd12a8
Your Name <you@example.com>
2026-03-07 12:07:42 +0000

    c

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+'''
+My module document
+'''
+import numpy as np
+
+
+class NeuralNetwork:
+    '''
+    My class document
+    '''
+    def __init__(self, nx, nodes):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b1 = np.zeros(shape=(nodes, 1))
+        self.__b2 = 0
+        self.__A1 = 0
+        self.__A2 = 0
+
+    @property
+    def W(self):
+        return self.__W1
+
+    @property
+    def b(self):
+        return self.__b1
+
+    @property
+    def A(self):
+        return self.__A1
+
+    @property
+    def W(self):
+        return self.__W2
+
+    @property
+    def b(self):
+        return self.__b2
+
+    @property
+    def A(self):
+        return self.__A2
