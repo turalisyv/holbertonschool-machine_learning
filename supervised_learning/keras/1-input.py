73bda287a8340b6be5314fabeb6afe0b6374d174
Your Name <you@example.com>
2026-03-11 12:33:43 +0000

    c

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""My module document"""
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """My function document"""
+    model = [K.layers.Input(shape=(nx,))]
+
+    for n, act in zip(layers, activations):
+        model.append(
+            K.layers.Dense(
+                units=n,
+                activation=act,
+                kernel_regularizer=K.regularizers.l2(lambtha)
+            )
+        )
+        model.append(
+            K.layers.Dropout(rate=1.0 - keep_prob)
+        )
+
+    model.pop()
+
+    return K.Sequential(model)
