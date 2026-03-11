8b79f04dcf0b22e45eb4142ec2b451dc82cbbc88
Your Name <you@example.com>
2026-03-11 13:06:19 +0000

    c

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -2,7 +2,7 @@
 """My module document"""
 import tensorflow.keras as K
 
-    
+
 def optimize_model(network, alpha, beta1, beta2):
     """My function document"""
     opt = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
