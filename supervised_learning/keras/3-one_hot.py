b915d440d23e65319ea83850d9b2e63ce18d5d5d
Your Name <you@example.com>
2026-03-11 13:18:16 +0000

    c

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """My module document"""
 import tensorflow.keras as K
-import tensorflow as tf
 
 
 def one_hot(labels, classes=None):
     """My function document"""
-    return tf.one_hot(labels, classes)
+    return tfK.ops.one_hot(labels, classes)
