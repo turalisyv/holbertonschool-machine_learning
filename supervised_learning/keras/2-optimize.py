63e8a571a587a184ca2d0b2a9d3467117588a532
Your Name <you@example.com>
2026-03-11 13:05:37 +0000

    c

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -6,6 +6,6 @@ import tensorflow.keras as K
 def optimize_model(network, alpha, beta1, beta2):
     """My function document"""
     opt = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
-    loss_fn = K.losses.CategoricalCrossentropy()
+    loss_fn = "categorical_crossentropy"
 
-    network.compile(optimizer=opt, loss=loss_fn,metrics=["accuracy"])
+    network.compile(optimizer=opt, loss=loss_fn, metrics=["accuracy"])
