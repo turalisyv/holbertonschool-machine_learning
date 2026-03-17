d9262c8f4e35400fb29773e835d9ede54371c2ff
Your Name <you@examples.com>
2026-03-17 09:45:58 +0000

    c

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""My module document"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+    """My function document"""
+    network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
