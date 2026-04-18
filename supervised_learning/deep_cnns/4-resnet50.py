9fb3c1fbcc37b3ffe10fbd0810f0afa7c00b7c81
Your Name <you@example.com>
2026-04-18 16:51:29 +0000

    c

---
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''My module document'''
+
+from tensorflow import keras as K
+
+def resnet50():
+    '''My class document''' 
+    initializer = K.initializers.he_normal(seed=0)
+
+    X_input = K.Input(shape=(224, 224, 3))
+
+    X = K.layers.ZeroPadding2D((3, 3))(X_input)
+
+    X = K.layers.Conv2D(64, (7, 7), strides=(2, 2),
+                        kernel_initializer=initializer)(X)
+    X = K.layers.BatchNormalization(axis=-1)(X)
+    X = K.layers.Activation('relu')(X)
+    X = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(X)
+
+    X = projection_block(X, [64, 64, 256], s=1)
+    X = identity_block(X, [64, 64, 256])
+    X = identity_block(X, [64, 64, 256])
+
+    X = projection_block(X, [128, 128, 512], s=2)
+    X = identity_block(X, [128, 128, 512])
+    X = identity_block(X, [128, 128, 512])
+    X = identity_block(X, [128, 128, 512])
+
+    X = projection_block(X, [256, 256, 1024], s=2)
+    X = identity_block(X, [256, 256, 1024])
+    X = identity_block(X, [256, 256, 1024])
+    X = identity_block(X, [256, 256, 1024])
+    X = identity_block(X, [256, 256, 1024])
+    X = identity_block(X, [256, 256, 1024])
+
+    X = projection_block(X, [512, 512, 2048], s=2)
+    X = identity_block(X, [512, 512, 2048])
+    X = identity_block(X, [512, 512, 2048])
+
+    X = K.layers.AveragePooling2D(pool_size=(7, 7))(X)
+
+    X = K.layers.Flatten()(X)
+    X = K.layers.Dense(1000, activation='softmax',
+                       kernel_initializer=initializer)(X)
+
+    model = K.models.Model(inputs=X_input, outputs=X)
+
+    return model
