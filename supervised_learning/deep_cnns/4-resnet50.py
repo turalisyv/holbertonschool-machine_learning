f2740becf04036eaca5060454418dbd4d30e1af1
Your Name <you@example.com>
2026-04-18 16:54:49 +0000

    c

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -7,14 +7,14 @@ projection_block = __import__('3-projection_block').projection_block
 
 def resnet50():
     '''My class document''' 
-    initializer = K.initializers.he_normal(seed=0)
+    init = K.initializers.he_normal(seed=0)
 
     X_input = K.Input(shape=(224, 224, 3))
 
     X = K.layers.ZeroPadding2D((3, 3))(X_input)
 
     X = K.layers.Conv2D(64, (7, 7), strides=(2, 2),
-                        kernel_initializer=initializer)(X)
+                        kernel_initializer=init)(X)
     X = K.layers.BatchNormalization(axis=-1)(X)
     X = K.layers.Activation('relu')(X)
     X = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(X)
@@ -43,7 +43,7 @@ def resnet50():
 
     X = K.layers.Flatten()(X)
     X = K.layers.Dense(1000, activation='softmax',
-                       kernel_initializer=initializer)(X)
+                       kernel_initializer=init)(X)
 
     model = K.models.Model(inputs=X_input, outputs=X)
 
