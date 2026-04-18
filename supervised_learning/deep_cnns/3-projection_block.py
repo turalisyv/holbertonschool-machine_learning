75e3400b2b94740e709dd6977acdcce476ec0dae
Your Name <you@example.com>
2026-04-18 16:49:50 +0000

    c

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -27,8 +27,8 @@ def projection_block(A_prev, filters, s=2):
     X = K.layers.BatchNormalization(axis=-1)(X)
 
     X_shortcut = K.layers.Conv2D(filters=F12, kernel_size=(1, 1),
-                                strides=(s, s), padding='valid',
-                                kernel_initializer=init)(X_shortcut)
+                                 strides=(s, s), padding='valid',
+                                 kernel_initializer=init)(X_shortcut)
     X_shortcut = K.layers.BatchNormalization(axis=-1)(X_shortcut)
 
     X = K.layers.Add()([X, X_shortcut])
