27a45925c3f6f298d1f20b7dfc68a69c3601dda3
Your Name <you@example.com>
2026-04-18 16:47:35 +0000

    c

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -7,7 +7,7 @@ from tensorflow import keras as K
 def projection_block(A_prev, filters, s=2):
     '''My class document'''
     F11, F3, F12 = filters
-    
+
     X_shortcut = A_prev
 
     init = K.initializers.he_normal(seed=0)
@@ -16,21 +16,21 @@ def projection_block(A_prev, filters, s=2):
                         padding='valid', kernel_initializer=init)(A_prev)
     X = K.layers.BatchNormalization(axis=-1)(X)
     X = K.layers.Activation('relu')(X)
-    
+
     X = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), strides=(1, 1),
                         padding='same', kernel_initializer=init)(X)
     X = K.layers.BatchNormalization(axis=-1)(X)
     X = K.layers.Activation('relu')(X)
-    
+
     X = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(1, 1),
                         padding='valid', kernel_initializer=init)(X)
     X = K.layers.BatchNormalization(axis=-1)(X)
-    
+
     X_shortcut = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(s, s),
                                  padding='valid', kernel_initializer=init)(X_shortcut)
     X_shortcut = K.layers.BatchNormalization(axis=-1)(X_shortcut)
-    
+
     X = K.layers.Add()([X, X_shortcut])
     X = K.layers.Activation('relu')(X)
-    
+
     return X
