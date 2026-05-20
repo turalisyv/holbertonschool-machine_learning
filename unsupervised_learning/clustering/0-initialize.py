faa7664585b6ac194028fbc6444097953d3c9e10
Your Name <you@example.com>
2026-05-20 11:38:57 +0000

    c

---
+++ unsupervised_learning/clustering/0-initialize.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""My module document"""
+import numpy as np
+
+
+def initialize(X, k):
+    """My function document"""
+    d = X.ndim
+    centroids = np.random.uniform(low=np.min(X, axis=0), high=np.max(X, axis=0), size=(k, d))
+
+    return centroids
