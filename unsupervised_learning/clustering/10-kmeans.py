6748c39ee6b86d58304f17ad739d0d3ed91d4868
Your Name <you@example.com>
2026-05-20 10:26:22 +0000

    c

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -2,11 +2,9 @@
 """My module document"""
 import sklearn.cluster
 
-
 def kmeans(X, k):
     """My function document"""
     model = sklearn.cluster.KMeans(n_clusters=k)
     model.fit(X)
 
     return model.cluster_centers_, model.predict(X)
-    
