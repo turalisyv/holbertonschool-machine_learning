06044eca4885bebf9bdba3fa0b3e0519caf8cda9
Your Name <you@example.com>
2026-05-20 11:48:57 +0000

    c

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -6,10 +6,21 @@ import numpy as np
 def initialize(X, k):
     """My function document"""
     try:
-        d = X.ndim
-        centroids = np.random.uniform(low=np.min(X, axis=0), high=np.max(X, axis=0), size=(k, d))
+        if not isinstance(X, np.ndarray) or X.ndim != 2:
+            return None
+
+        if not isinstance(k, int) or k <= 0 or k > X.shape[0]:
+            return None
+
+        d = X.shape[1]
+
+        low = np.min(X, axis=0)
+        high = np.max(X, axis=0)
+
+        centroids = np.random.uniform(
+            low=low,
+            high=high,
+            size=(k, d)
+        )
 
         return centroids
-
-    except ValueError:
-        return None
