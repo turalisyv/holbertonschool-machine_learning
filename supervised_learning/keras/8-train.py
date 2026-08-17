dd26f13c210ef033a130b6d0e615e39c158a3955
Your Name <you@example.com>
2026-08-17 04:23:22 +0400

    c

--- supervised_learning/keras/8-train.py
+++ supervised_learning/keras/8-train.py
@@ -7,6 +7,7 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size,
                 epochs, validation_data=None, early_stopping=False, patience=0,
                 learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                save_best=False, filepath=None,
                 verbose=True, shuffle=False):
     """Train model"""
     callbacks = []
@@ -27,6 +28,15 @@ def train_model(network, data, labels, batch_size,
         lr_decay = K.callbacks.LearningRateScheduler(schedule, verbose=1)
         callbacks.append(lr_decay)
 
+    if save_best and filepath is not None:
+        checkpoint = K.callbacks.ModelCheckpoint(
+            filepath=filepath,
+            monitor='val_loss',
+            mode='min',
+            save_best_only=save_best,
+        )
+        callbacks.append(checkpoint)
+
     history = network.fit(
         data,
         labels,
@@ -37,4 +47,5 @@ def train_model(network, data, labels, batch_size,
         shuffle=shuffle,
         callbacks=callbacks
     )
+
     return history
